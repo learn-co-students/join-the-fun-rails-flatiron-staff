c7e5f8c70c2aa95534986f614522ded82480416d
github username <github email address>
2018-12-01 21:50:11 -0500

    Done.

---
+++ db/migrate/001_create_taxis_passengers.rb
@@ -0,0 +1,8 @@
+class CreateTaxisPassengers < ActiveRecord::Migration
+  def change
+    create_join_table :taxis, :passengers do |t|
+      t.index :taxi_id
+      t.index :passenger_id
+    end
+  end
+end
